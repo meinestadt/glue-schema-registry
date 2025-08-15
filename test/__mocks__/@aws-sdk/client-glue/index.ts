914fce8a461a80df78b45c21fc470ac3258de751
Felix Ritter <felix.ritter@gmail.com>
2025-08-15 14:51:07 +0200

    adjust delay for mock aws api requests to speed up the unit tests

--- test/__mocks__/@aws-sdk/client-glue/index.ts
+++ test/__mocks__/@aws-sdk/client-glue/index.ts
@@ -13,7 +13,7 @@ type GlueCommandMock<Input, Output> = jest.MockedFunction<(params: Input) => Pro
 
 const mockedSend = jest.fn().mockImplementation((command: unknown) => {
   return new Promise((resolve) => {
-    const delay = Math.floor(Math.random() * 500) + 500 // 50–550 ms
+    const delay = Math.floor(Math.random() * 50) + 250 // 50–250 ms
     setTimeout(() => {      
       resolve(command)
     }, delay)
